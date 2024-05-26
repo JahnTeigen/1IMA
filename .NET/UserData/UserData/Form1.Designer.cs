9b2509c48d2ed7d1c4d87e6e91a86d07a67d50ca
JahnTeigen <157064719+JahnTeigen@users.noreply.github.com>
2024-05-26 13:52:52 +0200

    Update Form1.Designer.cs

--- .NET/UserData/UserData/Form1.Designer.cs
+++ .NET/UserData/UserData/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace UserData
+namespace UserData
 {
     partial class Form1
     {
@@ -31,46 +31,164 @@
             btnRegister = new Button();
             txtbUsername = new TextBox();
             txtbPassword = new TextBox();
+            splitContainer1 = new SplitContainer();
+            lblRegistration = new Label();
+            txtbSurname = new TextBox();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            tabControl1.SuspendLayout();
             SuspendLayout();
             // 
             // btnRegister
             // 
-            btnRegister.Location = new Point(12, 71);
+            btnRegister.BackColor = SystemColors.ActiveCaption;
+            btnRegister.Location = new Point(12, 307);
             btnRegister.Name = "btnRegister";
-            btnRegister.Size = new Size(100, 26);
+            btnRegister.Size = new Size(175, 34);
             btnRegister.TabIndex = 0;
-            btnRegister.Text = "button1";
-            btnRegister.UseVisualStyleBackColor = true;
+            btnRegister.Text = "Register User";
+            btnRegister.UseVisualStyleBackColor = false;
             btnRegister.Click += btnRegister_Click;
             // 
             // txtbUsername
             // 
-            txtbUsername.Location = new Point(12, 12);
+            txtbUsername.Location = new Point(12, 75);
             txtbUsername.Name = "txtbUsername";
-            txtbUsername.Size = new Size(100, 23);
+            txtbUsername.Size = new Size(175, 23);
             txtbUsername.TabIndex = 1;
             txtbUsername.TextChanged += txtbUsername_TextChanged;
             // 
             // txtbPassword
             // 
-            txtbPassword.Location = new Point(12, 41);
+            txtbPassword.Location = new Point(12, 161);
             txtbPassword.Name = "txtbPassword";
-            txtbPassword.Size = new Size(100, 23);
+            txtbPassword.Size = new Size(175, 23);
             txtbPassword.TabIndex = 1;
             txtbPassword.TextChanged += txtbPassword_TextChanged;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(12, 12);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(label3);
+            splitContainer1.Panel1.Controls.Add(label2);
+            splitContainer1.Panel1.Controls.Add(label1);
+            splitContainer1.Panel1.Controls.Add(lblRegistration);
+            splitContainer1.Panel1.Controls.Add(btnRegister);
+            splitContainer1.Panel1.Controls.Add(txtbUsername);
+            splitContainer1.Panel1.Controls.Add(txtbSurname);
+            splitContainer1.Panel1.Controls.Add(txtbPassword);
+            splitContainer1.Panel1.Paint += splitContainer1_Panel1_Paint;
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(tabControl1);
+            splitContainer1.Size = new Size(601, 354);
+            splitContainer1.SplitterDistance = 200;
+            splitContainer1.TabIndex = 2;
+            // 
+            // lblRegistration
+            // 
+            lblRegistration.AutoSize = true;
+            lblRegistration.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblRegistration.Location = new Point(12, 13);
+            lblRegistration.Name = "lblRegistration";
+            lblRegistration.Size = new Size(175, 37);
+            lblRegistration.TabIndex = 2;
+            lblRegistration.Text = "Registration";
+            lblRegistration.Click += label1_Click;
+            // 
+            // txtbSurname
+            // 
+            txtbSurname.Location = new Point(12, 118);
+            txtbSurname.Name = "txtbSurname";
+            txtbSurname.Size = new Size(175, 23);
+            txtbSurname.TabIndex = 1;
+            txtbSurname.TextChanged += txtbPassword_TextChanged;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(3, 3);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(394, 351);
+            tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(386, 323);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(386, 323);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 57);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 3;
+            label1.Text = "First name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 143);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Password";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(625, 378);
-            Controls.Add(txtbPassword);
-            Controls.Add(txtbUsername);
-            Controls.Add(btnRegister);
+            Controls.Add(splitContainer1);
             Name = "Form1";
             Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            tabControl1.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -78,5 +196,14 @@
         private Button btnRegister;
         private TextBox txtbUsername;
         private TextBox txtbPassword;
+        private SplitContainer splitContainer1;
+        private Label lblRegistration;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TextBox txtbSurname;
+        private Label label2;
+        private Label label1;
+        private Label label3;
     }
 }
