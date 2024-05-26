6c49869fc9d96264e52e6dd899dd4a860c754967
JahnTeigen <157064719+JahnTeigen@users.noreply.github.com>
2024-05-26 13:50:11 +0200

    Update Form1.cs

--- .NET/UserData/UserData/Form1.cs
+++ .NET/UserData/UserData/Form1.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 
 namespace UserData
 {
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -19,11 +20,20 @@ namespace UserData
                 try
                 {
                     connection.Open();
-                    string query = "INSERT INTO UserData (username, password) VALUES (@username, @password)";
+
+                    // SQL query with positional parameters
+                    string query = "INSERT INTO UserData ([firstname], [surname], [admin], [password]) VALUES (?, ?, ?, ?)";
                     using (OleDbCommand command = new OleDbCommand(query, connection))
                     {
-                        command.Parameters.AddWithValue("@username", txtbUsername.Text);
-                        command.Parameters.AddWithValue("@password", txtbPassword.Text);
+                        // Add parameters in the order they appear in the SQL query
+                        command.Parameters.AddWithValue("?", txtbUsername.Text); // Username/firstname
+                        command.Parameters.AddWithValue("?", txtbSurname.Text); // Surname
+                        command.Parameters.AddWithValue("?", false);
+                        command.Parameters.AddWithValue("?", txtbPassword.Text); // Password
+
+                        txtbUsername.Text = "";
+                        txtbSurname.Text = "";
+                        txtbPassword.Text = "";
                         int result = command.ExecuteNonQuery();
 
                         if (result > 0)
@@ -36,11 +46,20 @@ namespace UserData
                         }
                     }
                 }
+                catch (OleDbException ex)
+                {
+                    MessageBox.Show("A database error occurred: " + ex.Message);
+                }
                 catch (Exception ex)
                 {
                     MessageBox.Show("An error occurred: " + ex.Message);
                 }
+                finally
+                {
+                    connection.Close();
+                }
             }
+
         }
 
         private void txtbUsername_TextChanged(object sender, EventArgs e)
@@ -49,6 +68,21 @@ namespace UserData
         }
 
         private void txtbPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
         {
 
         }
